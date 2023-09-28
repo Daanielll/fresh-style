55600e66b789547ae43145ec4eca76df4dafb7f4
Daniel Moses <dm98912@gmail.com>
2023-09-29 00:57:27 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Header } from "./components/header";
+import { Header } from "./components/Header";
 import { HomeScreen } from "./components/HomeScreen";
 import "./App.css";
 import { Footer } from "./components/Footer";
